5c67231dbf2e7dfd88ef1c1543524a2837c355a6
David Wheatley <blokmaster.help@gmail.com>
2019-03-25 23:30:49 +0000

    v2.2: Fix broken intro wiki page

--- src/constants/DocShortLinks.jsx
+++ src/constants/DocShortLinks.jsx
@@ -1,6 +1,7 @@
 export const prefix = "https://w24.page.link/";
 
 export const docs = {
+  "": "wiki",
   creating_a_pinout_cable: "pinout",
   flashing_the_rom_and_kernel: "flash",
   flashing_twrp_recovery: "twrp",
